7fdbc633e95676cded6cd051e8359188bffafd87
Valery Keplin <valerykeplin@gmail.com>
2025-07-25 15:15:57 +0400

    Initial commit

---
+++ AGPlayer/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let vc = ViewController()
+        let navigationController = UINavigationController(rootViewController: vc)
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
---
+++ AGPlayer/Modules/Search/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  AGPlayer
+//
+//  Created by Valery Keplin on 25.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
