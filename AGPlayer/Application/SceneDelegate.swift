512f458792eee449c632a78e46c58430530a8a1f
Valery Keplin <valerykeplin@gmail.com>
2025-07-28 12:28:18 +0400

    feat(Project)[#####]: Setup a light theme only

--- AGPlayer/Application/SceneDelegate.swift
+++ AGPlayer/Application/SceneDelegate.swift
@@ -12,6 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
+        window.overrideUserInterfaceStyle = .light
         let startVC = StartModuleBuilder.build()
         window.rootViewController = startVC
         self.window = window
